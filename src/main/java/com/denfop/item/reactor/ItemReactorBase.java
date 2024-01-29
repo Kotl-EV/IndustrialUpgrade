1b8578f984169ac263d35fd7585e741aa16e5049
Kotl-EV <74097230+Kotl-EV@users.noreply.github.com>
2024-01-30 02:11:03 +0300

    фикс краша с реакторными стержнями

--- src/main/java/com/denfop/item/reactor/ItemReactorBase.java
+++ src/main/java/com/denfop/item/reactor/ItemReactorBase.java
@@ -7,32 +7,27 @@ import ic2.api.reactor.IReactorComponent;
 import ic2.core.IC2Potion;
 import ic2.core.Ic2Items;
 import ic2.core.item.armor.ItemArmorHazmat;
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.Queue;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
-import java.util.ArrayDeque;
-import java.util.Collection;
-import java.util.Queue;
-
 public class ItemReactorBase extends ReactorItemCore implements IReactorComponent {
     public final int numberOfCells;
 
-
     protected ItemReactorBase(String internalName, int cells, int duration) {
         super(internalName, duration);
-        setMaxStackSize(64);
+        this.setMaxStackSize(64);
         this.numberOfCells = cells;
         this.setCreativeTab(IUCore.tabssp3);
     }
 
     protected static int checkPulseable(IReactor reactor, int x, int y, ItemStack me, int mex, int mey) {
         ItemStack other = reactor.getItemAt(x, y);
-        if (other != null && other.getItem() instanceof IReactorComponent && ((IReactorComponent) other.getItem())
-                .acceptUraniumPulse(reactor, other, me, x, y, mex, mey, true))
-            return 1;
-        return 0;
+        return other != null && other.getItem() instanceof IReactorComponent && ((IReactorComponent)other.getItem()).acceptUraniumPulse(reactor, other, me, x, y, mex, mey, true) ? 1 : 0;
     }
 
     protected static int triangularNumber(int x) {
@@ -40,42 +35,47 @@ public class ItemReactorBase extends ReactorItemCore implements IReactorComponen
     }
 
     public void processChamber(IReactor reactor, ItemStack stack, int x, int y, boolean heatRun) {
-        if (!reactor.produceEnergy())
-            return;
-        int basePulses = 1 + this.numberOfCells / 2;
-        for (int iteration = 0; iteration < this.numberOfCells; iteration++) {
-            int pulses = basePulses;
-            if (!heatRun) {
-                for (int i = 0; i < pulses; i++)
-                    acceptUraniumPulse(reactor, stack, stack, x, y, x, y, false);
-            } else {
-                pulses += checkPulseable(reactor, x - 1, y, stack, x, y)
-                        + checkPulseable(reactor, x + 1, y, stack, x, y)
-                        + checkPulseable(reactor, x, y - 1, stack, x, y)
-                        + checkPulseable(reactor, x, y + 1, stack, x, y);
-                int heat = triangularNumber(pulses) * 4;
-                heat = getFinalHeat(reactor, heat);
-                Queue<ItemStackCoord> heatAcceptors = new ArrayDeque<>();
-                checkHeatAcceptor(reactor, x - 1, y, heatAcceptors);
-                checkHeatAcceptor(reactor, x + 1, y, heatAcceptors);
-                checkHeatAcceptor(reactor, x, y - 1, heatAcceptors);
-                checkHeatAcceptor(reactor, x, y + 1, heatAcceptors);
-                while (!heatAcceptors.isEmpty() && heat > 0) {
-                    int dheat = heat / heatAcceptors.size();
-                    heat -= dheat;
-                    ItemStackCoord acceptor = heatAcceptors.remove();
-                    IReactorComponent acceptorComp = (IReactorComponent) acceptor.stack.getItem();
-                    dheat = acceptorComp.alterHeat(reactor, acceptor.stack, acceptor.x, acceptor.y, dheat);
-                    heat += dheat;
+        if (reactor.produceEnergy()) {
+            int basePulses = 1 + this.numberOfCells / 2;
+
+            for(int iteration = 0; iteration < this.numberOfCells; ++iteration) {
+                int pulses = basePulses;
+                int heat;
+                if (!heatRun) {
+                    for(heat = 0; heat < pulses; ++heat) {
+                        this.acceptUraniumPulse(reactor, stack, stack, x, y, x, y, false);
+                    }
+                } else {
+                    pulses = basePulses + checkPulseable(reactor, x - 1, y, stack, x, y) + checkPulseable(reactor, x + 1, y, stack, x, y) + checkPulseable(reactor, x, y - 1, stack, x, y) + checkPulseable(reactor, x, y + 1, stack, x, y);
+                    heat = triangularNumber(pulses) * 4;
+                    heat = this.getFinalHeat(reactor, heat);
+                    Queue<ItemReactorBase.ItemStackCoord> heatAcceptors = new ArrayDeque();
+                    this.checkHeatAcceptor(reactor, x - 1, y, heatAcceptors);
+                    this.checkHeatAcceptor(reactor, x + 1, y, heatAcceptors);
+                    this.checkHeatAcceptor(reactor, x, y - 1, heatAcceptors);
+                    this.checkHeatAcceptor(reactor, x, y + 1, heatAcceptors);
+
+                    while(!heatAcceptors.isEmpty() && heat > 0) {
+                        int dheat = heat / heatAcceptors.size();
+                        heat -= dheat;
+                        ItemReactorBase.ItemStackCoord acceptor = (ItemReactorBase.ItemStackCoord)heatAcceptors.remove();
+                        IReactorComponent acceptorComp = (IReactorComponent)acceptor.stack.getItem();
+                        dheat = acceptorComp.alterHeat(reactor, acceptor.stack, acceptor.x, acceptor.y, dheat);
+                        heat += dheat;
+                    }
+
+                    if (heat > 0) {
+                        reactor.addHeat(heat);
+                    }
                 }
-                if (heat > 0)
-                    reactor.addHeat(heat);
             }
-        }
-        if (getCustomDamage(stack) >= getMaxCustomDamage(stack) - 1) {
-            reactor.setItemAt(x, y, getDepletedStack());
-        } else if (heatRun) {
-            applyCustomDamage(stack, 1, null);
+
+            if (this.getCustomDamage(stack) >= this.getMaxCustomDamage(stack) - 1) {
+                reactor.setItemAt(x, y, this.getDepletedStack());
+            } else if (heatRun) {
+                this.applyCustomDamage(stack, 1, (EntityLivingBase)null);
+            }
+
         }
     }
 
@@ -85,31 +85,35 @@ public class ItemReactorBase extends ReactorItemCore implements IReactorComponen
 
     protected ItemStack getDepletedStack() {
         ItemStack ret;
-        switch (this.numberOfCells) {
+        switch(this.numberOfCells) {
             case 1:
                 ret = Ic2Items.reactorDepletedUraniumSimple;
                 return new ItemStack(ret.getItem(), 1);
             case 2:
                 ret = Ic2Items.reactorDepletedUraniumDual;
                 return new ItemStack(ret.getItem(), 1);
+            case 3:
+            default:
+                throw new RuntimeException("invalid cell count: " + this.numberOfCells);
             case 4:
                 ret = Ic2Items.reactorDepletedUraniumQuad;
                 return new ItemStack(ret.getItem(), 1);
         }
-        throw new RuntimeException("invalid cell count: " + this.numberOfCells);
     }
 
-    protected void checkHeatAcceptor(IReactor reactor, int x, int y, Collection<ItemStackCoord> heatAcceptors) {
+    protected void checkHeatAcceptor(IReactor reactor, int x, int y, Collection<ItemReactorBase.ItemStackCoord> heatAcceptors) {
         ItemStack thing = reactor.getItemAt(x, y);
-        if (thing != null && thing.getItem() instanceof IReactorComponent
-                && ((IReactorComponent) thing.getItem()).canStoreHeat(reactor, thing, x, y))
-            heatAcceptors.add(new ItemStackCoord(thing, x, y));
+        if (thing != null && thing.getItem() instanceof IReactorComponent && ((IReactorComponent)thing.getItem()).canStoreHeat(reactor, thing, x, y)) {
+            heatAcceptors.add(new ItemReactorBase.ItemStackCoord(thing, x, y));
+        }
+
     }
 
-    public boolean acceptUraniumPulse(IReactor reactor, ItemStack yourStack, ItemStack pulsingStack, int youX, int youY,
-                                      int pulseX, int pulseY, boolean heatrun) {
-        if (!heatrun)
+    public boolean acceptUraniumPulse(IReactor reactor, ItemStack yourStack, ItemStack pulsingStack, int youX, int youY, int pulseX, int pulseY, boolean heatrun) {
+        if (!heatrun) {
             reactor.addOutput(1.0F);
+        }
+
         return true;
     }
 
@@ -130,22 +134,22 @@ public class ItemReactorBase extends ReactorItemCore implements IReactorComponen
     }
 
     public float influenceExplosion(IReactor reactor, ItemStack yourStack) {
-        return (2 * this.numberOfCells);
+        return (float)(2 * this.numberOfCells);
     }
 
     public void onUpdate(ItemStack stack, World world, Entity entity, int slotIndex, boolean isCurrentItem) {
         if (entity instanceof EntityLivingBase) {
-            EntityLivingBase entityLiving = (EntityLivingBase) entity;
-            if (!ItemArmorHazmat.hasCompleteHazmat(entityLiving))
+            EntityLivingBase entityLiving = (EntityLivingBase)entity;
+            if (!ItemArmorHazmat.hasCompleteHazmat(entityLiving)) {
                 IC2Potion.radiation.applyTo(entityLiving, 200, 100);
+            }
         }
+
     }
 
     private static class ItemStackCoord {
         public final ItemStack stack;
-
         public final int x;
-
         public final int y;
 
         public ItemStackCoord(ItemStack stack, int x, int y) {
